e06579db933ba7b1be293413c2d3d418ecba9782
Mark Silverberg <mark.silverberg@socrata.com>
2016-04-16 14:53:32 -0400

    fixes

--- flattener/flatten_from_index.py
+++ flattener/flatten_from_index.py
@@ -1,5 +1,3 @@
-# TODO - plans
-
 # import dependencies
 import time
 import datetime
@@ -26,7 +24,7 @@ def get_json_from_url(url):
             payload_size = size(int(head.headers['Content-Length']))
         except: 
             payload_size = "UNKNOWN"
-        logging.info("... HTTP GET'ing: {0} [the file size is {1}".format(url, payload_size))
+        logging.info("... HTTP GET'ing: {0} [the file size is {1}]".format(url, payload_size))
         data = requests.get(url)
         data = data.json()
     except:
@@ -37,7 +35,9 @@ def get_json_from_url(url):
 
 array_of_index_files = [
     "https://fm.formularynavigator.com/jsonFiles/publish/11/47/cms-data-index.json",
-    "https://www.deltadental.com/CMSDirectory/index.json",
+    "https://www.gundersenhealthplan.org/QHPAPI/PNM/index.json",
+    "https://www.caresource.com/vendor/cms/cms-data-index.json",
+    "https://api.centene.com/ambetter/reference/cms-data-index.json"
 ]
 
 all_drugs = []
@@ -45,37 +45,58 @@ all_plans = []
 all_providers = []
 
 drug_fields_to_include = ['rxnorm_id','drug_name']
+
 provider_fields_to_include = ['npi','type','last_updated_on','phone','accepting','gender','facility_name']
 provider_nested_fields_to_concatenate = ['addresses', 'specialty', 'languages', 'facility_type']
 provider_nested_fields_in_name = ['prefix','first','middle','last','suffix']
+
+plan_fields_to_include = ['plan_id_type','plan_id','marketing_name','summary_url','marketing_url','formulary_url','plan_contact','last_updated_on']
+plan_nested_fields_to_concatenate = ['network','formulary','benefits']
+
+
 # take a index.json file, fetch it
 for index_url in array_of_index_files:
     logging.info("About to fetch index.json: {0}".format(index_url))
     index_json = get_json_from_url(index_url)
     
-    # # for all URLs in formulary_urls array, flatten into one array
-    # for formulary_url in index_json["formulary_urls"]:
-    #     logging.info("About to fetch formulary.json: {0}".format(formulary_url))
-    #     formulary_json = get_json_from_url(formulary_url)
-    #     logging.info("... processing formulary.json: {0}".format(formulary_url))
-    #     for drug in formulary_json:
-    #         for plan in drug["plans"]:
-    #             drug_plan_dict = {
-    #                 '_index_url': index_url,
-    #                 '_formulary_url': formulary_url
-    #             }
-    #            for field in drug_fields_to_include:
-    #                provider_plan_dict[field] = provider.get(field)
-    #             drug_plan_dict.update(plan)
-    #             all_drugs.append(drug_plan_dict)
+    # for all URLs in formulary_urls array, flatten into one array
+    for formulary_url in index_json["formulary_urls"]:
+        logging.info("About to fetch formulary.json: {0}".format(formulary_url))
+        formulary_json = get_json_from_url(formulary_url)
+        logging.info("... processing formulary.json: {0}".format(formulary_url))
+        for drug in formulary_json:
+            for plan in drug["plans"]:
+                drug_plan_dict = {
+                    '_index_url': index_url,
+                    '_formulary_url': formulary_url
+                }
+                for field in drug_fields_to_include:
+                    drug_plan_dict[field] = drug.get(field)
+            drug_plan_dict.update(plan)
+            all_drugs.append(drug_plan_dict)
 
 
-    # # for all URLs in plan_urls array, flatten into one array
-    # for plan_url in index_json["plan_urls"]:
-    #     logging.info("About to fetch plans.json: {0}".format(plan_url))
-    #     # plan_json = get_json_from_url(formulary_url)
-    #     logging.info("... processing plan.json: {0}".format(plan_url))
-
+    # for all URLs in plan_urls array, flatten into one array
+    for plan_url in index_json["plan_urls"]:
+        logging.info("About to fetch plans.json: {0}".format(plan_url))
+        plan_json = get_json_from_url(plan_url)
+        logging.info("... processing plan.json: {0}".format(plan_url))
+        for plan in plan_json:
+            plan_dict = {
+                '_index_url': index_url,
+                '_plan_url': plan_url,
+            }
+        for field in plan_fields_to_include:
+            plan_dict[field] = plan.get(field)
+        for field in plan_nested_fields_to_concatenate:
+            field_value = plan.get(field)
+            if(field_value == None):
+                plan_dict['_n_{0}'.format(field)] = 0
+                plan_dict[field] = None
+            else:
+                plan_dict['_n_{0}'.format(field)] = len(field_value)
+                plan_dict[field] = json.dumps(field_value)
+        all_plans.append(plan_dict)
 
     # for all URLs in provider_urls array, flatten into one array
     for provider_url in index_json["provider_urls"]:
@@ -109,20 +130,23 @@ for index_url in array_of_index_files:
 
 # Save compiled data into CSV and/or JSON files
 files_to_generate = [
-    # {'name': 'all_drugs', 'data': all_drugs, 'csv': True, 'json': False},
-    # {'name': 'all_plans', 'data': all_plans, 'csv': True, 'json': False}
+    {'name': 'all_drugs', 'data': all_drugs, 'csv': True, 'json': False},
+    {'name': 'all_plans', 'data': all_plans, 'csv': True, 'json': False},
     {'name': 'all_providers', 'data': all_providers, 'csv': True, 'json': False}
 ]
 
 for file_info in files_to_generate:
-    logging.debug('Saving {0} to {0}.json and {0}.csv'.format(file_info['name']))
-    if file_info['json']: # save to JSON
-        f = open('{0}.json'.format(file_info['name']), 'w')
-        print >> f, file_info['data']
-        f.close()
-    if file_info['csv']: # save to CSV      
-        keys = file_info['data'][0].keys()
-        with open('{0}.csv'.format(file_info['name']), 'wb') as output_file:
-            dict_writer = csv.DictWriter(output_file, keys)
-            dict_writer.writeheader()
-            dict_writer.writerows(file_info['data'])
+    logging.info('Saving {0} to {0}.json and {0}.csv'.format(file_info['name']))
+    if(len(file_info['data']) > 0):
+        if file_info['json']: # save to JSON
+            f = open('{0}.json'.format(file_info['name']), 'w')
+            print >> f, file_info['data']
+            f.close()
+        if file_info['csv']: # save to CSV      
+            keys = file_info['data'][0].keys()
+            with open('{0}.csv'.format(file_info['name']), 'wb') as output_file:
+                dict_writer = csv.DictWriter(output_file, keys)
+                dict_writer.writeheader()
+                dict_writer.writerows(file_info['data'])
+    else: 
+        logging.info('... did not save, there were no records to save')
